2212003ebcbcd0e8ce53a05f1ce1bd6d6e126083
Atul <atul007414@gmail.com>
2026-08-20 16:38:56 +0530

    fix: fastify returns string instead pass fastify server to web sockets

--- backend/src/api/server.ts
+++ backend/src/api/server.ts
@@ -3,20 +3,17 @@ import { config } from './config/env.js';
 import { initKafkaInfrastructure } from '../kafka-infrastructure/index.js';
 import { initWebSocketServer } from '../ws/index.js';
 
-let httpServer: any;
-
 const startServer = async () => {
     try {
         await initKafkaInfrastructure();
 
-        httpServer = await app.listen({
+        await app.listen({
             port: parseInt(config.PORT),
             host: '0.0.0.0'
         });
         console.log(`Server is running on port ${config.PORT}`);
 
-        // Attach WS server now that httpServer is ready
-        initWebSocketServer(httpServer);
+        initWebSocketServer(app.server);
     } catch (error) {
         console.log(error);
         process.exit(1);
@@ -24,4 +21,3 @@ const startServer = async () => {
 }
 
 startServer();
-export { httpServer }
